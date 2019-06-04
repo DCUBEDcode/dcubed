ff5046a69d0da4b3eef6bc1fde35b1ec95dd3f12
Jason <jason@dcubed.com.au>
2019-06-04 21:48:12 +1000

    Update landing page navigation for mobile

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO
+
+- SEO structure of the page
+- Full 'Our work' page
+- Get a proposal form
+- mobile nav
---
+++ app.js
@@ -0,0 +1,38 @@
+const header = document.getElementById('header');
+
+const mobileNav = document.getElementById('mobile-nav');
+const mobileNavLogo = document.getElementById('mobile-nav-logo');
+
+const howImages = document.querySelectorAll('.how-we-work__img');
+
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+function scrollHandler(event) {
+  let scrollTop = event.target.scrollingElement.scrollTop;
+  if(scrollTop > document.body.offsetHeight) {
+    mobileNav.classList.add('mobile-nav--show');
+  } else {
+    mobileNav.classList.remove('mobile-nav--show');
+  }
+
+  howImages.forEach((el, i) => {
+    if (isInViewport(el) && !el.classList.contains('how-we-work__img--show')) {
+      el.classList.add('how-we-work__img--show');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollHandler);
+
+mobileNavLogo.addEventListener('click', (event) => {
+  event.preventDefault();
+  mobileNav.classList.toggle('mobile-nav--open');
+})
